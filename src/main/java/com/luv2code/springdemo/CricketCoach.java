05f3c4ffee72c85426c3a4c241f0d67af73cb6e2
riyad <riyad.hennaoui@gmail.com>
2022-02-28 17:56:17 +0100

    Spring Dependency injection with Literal Values

--- src/main/java/com/luv2code/springdemo/CricketCoach.java
+++ src/main/java/com/luv2code/springdemo/CricketCoach.java
@@ -3,6 +3,8 @@ package com.luv2code.springdemo;
 public class CricketCoach implements Coach{
 
     private FortuneService fortuneService;
+    private String emailAddress;
+    private String team;
 
     // create a no-arg constructor
     public CricketCoach() {
@@ -13,6 +15,24 @@ public class CricketCoach implements Coach{
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println("CricketCoach: inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
     }
 
     @Override
