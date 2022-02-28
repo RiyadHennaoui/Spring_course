7876422766113a09f9ec1425214ed31b6d9a6f7a
riyad <riyad.hennaoui@gmail.com>
2022-02-28 17:38:57 +0100

    String Dependency Injection with setter

--- src/main/java/com/luv2code/springdemo/CricketCoach.java
+++ src/main/java/com/luv2code/springdemo/CricketCoach.java
@@ -1,2 +1,27 @@
-package com.luv2code.springdemo;public class CricketCoach {
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach{
+
+    private FortuneService fortuneService;
+
+    // create a no-arg constructor
+    public CricketCoach() {
+        System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
+    // for name of property in applicationContext.xml we need to leave set before the name of methode and cancel capitalize of first letter
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("CricketCoach: inside setter method - setFortuneService");
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
 }
